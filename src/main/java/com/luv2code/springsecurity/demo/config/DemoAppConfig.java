590907ae4f9513c011441a6ebd57033939f1f181
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-08-21 13:35:52 +0300

    progress update

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -18,7 +18,7 @@ public class DemoAppConfig {
 		
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		
-		viewResolver.setPrefix("/WEB-INF/view");
+		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+	<title> Company home page</title>
+</head>
+
+<body>
+	<h2>Company home page</h2>
+	<hr>
+	
+	Welcome to the company home page!
+</body>
+
+</html>
